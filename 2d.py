0115e09af7184904e41e1837d27a728dfeb78427
James Camacho <camacho.james@gmail.com>
2024-09-11 22:41:30 -0700

    Success for Dirichlet boundary!

--- 2d.py
+++ 2d.py
@@ -1,6 +1,6 @@
 from mpi4py import MPI
 from slepc4py import SLEPc
-from petsc4py import PETSc
+from petsc4py import PETSc, init
 import dolfinx.fem.petsc
 from dolfinx import fem
 from dolfinx.plot import vtk_mesh
@@ -9,6 +9,7 @@ import os, shutil
 import numpy as np
 import pyvista as pv
 import matplotlib.pyplot as plt
+import sys
 
 from constants import *
 import ufl
@@ -20,21 +21,26 @@ if os.path.exists(cache_dir):
     print(f"Cleared FEniCS cache: {cache_dir}")
 
 # Read mesh
-domain, cell_tags, facet_tags = gmshio.read_from_msh("resonator.msh", MPI.COMM_WORLD, gdim=2)
+domain, cell_tags, facet_tags = gmshio.read_from_msh("mesh/resonator.msh", MPI.COMM_WORLD, gdim=2)
 
 # Function space
-V = fem.FunctionSpace(domain, ("CG", 1))
+V = fem.VectorFunctionSpace(domain, ("CG", 1), dim=2)
+
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
 
 # Relative permittivity
-epsilon_r = fem.Function(V)
+epsilon_r = fem.Function(V.sub(0).collapse()[0])
 air_dofs = fem.locate_dofs_topological(V, domain.topology.dim, cell_tags.find(2))
 ceramic_dofs = fem.locate_dofs_topological(V, domain.topology.dim, cell_tags.find(1))
 epsilon_r.x.array[air_dofs] = 1
 epsilon_r.x.array[ceramic_dofs] = EPSILON_R
 
 # Trial and test function
-u = TrialFunction(V)
-v = conj(TestFunction(V))
+uz, ur = ufl.split(u)
+vz, vr = ufl.split(v)
+vz = conj(vz)
+vr = conj(vr)
 
 # Coordinate system
 x = SpatialCoordinate(domain)
@@ -45,11 +51,11 @@ ds = ufl.Measure("ds", domain=domain, subdomain_data=facet_tags)
 daxis = ufl.Measure("ds", domain=domain, subdomain_data=facet_tags)
 
 # Define the weak form
-a = r * dot(grad(u), grad(v)) * dx + \
-    1 / r * u * v * dx - \
-    1j * r * u * v * ds(3)
+a = r * inner(grad(u), grad(v)) * dx + \
+    1 / r * ur * vr * dx - \
+    0 #1j * r * inner(u, v) * ds(3)
 
-m = r * epsilon_r * u * v * dx
+m = r * epsilon_r * inner(u, v) * dx
 
 # Dirichlet boundary
 boundary_dofs = fem.locate_dofs_topological(V, domain.topology.dim-1, entities=facet_tags.find(3))
@@ -57,8 +63,8 @@ u_bc = fem.Function(V)
 u_bc.x.array[:] = 0
 
 bc = fem.dirichletbc(u_bc, boundary_dofs)
-# bcs = [bc]
-bcs = []
+bcs = [bc]
+# bcs = []
 
 # Assemble matrices
 A = fem.petsc.assemble_matrix(fem.form(a), bcs=bcs)
@@ -74,13 +80,11 @@ eps = SLEPc.EPS().create(domain.comm)
 eps.setOperators(A, M)
 eps.setProblemType(SLEPc.EPS.ProblemType.GNHEP)
 eps.setDimensions(50)
-eps.setWhichEigenpairs(SLEPc.EPS.Which.TARGET_MAGNITUDE)
+eps.setWhichEigenpairs(SLEPc.EPS.Which.SMALLEST_MAGNITUDE)
 eps.setTarget(target)
-# eps.setTolerances(1e-10, 20)
-# eps.setType(SLEPc.EPS.Type.POWER)
 
 st = eps.getST()
-st.setType(SLEPc.ST.Type.SINVERT)
+# st.setType(SLEPc.ST.Type.SINVERT)
 
 # Set the shift to your target value
 st.setShift(target)
@@ -91,12 +95,8 @@ ksp.setType(PETSc.KSP.Type.PREONLY)
 pc = ksp.getPC()
 pc.setType(PETSc.PC.Type.LU)
 
-# If you're dealing with a large problem, you might want to use an iterative solver instead:
-# ksp.setType(PETSc.KSP.Type.GMRES)
-# pc.setType(PETSc.PC.Type.ILU)
-
 # Optionally, you can set a convergence test
-eps.setConvergenceTest(SLEPc.EPS.Conv.REL)
+# eps.setConvergenceTest(SLEPc.EPS.Conv.REL)
 
 # Now you can call eps.solve()
 eps.solve()
@@ -105,112 +105,47 @@ nconv = eps.getConverged()
 print("Total converged", nconv)
 
 # Extract eigenvalues and eigenvectors
-# for i in range(nconv):
-i = 0
-eigval = eps.getEigenvalue(i)
-f = np.sqrt(eigval.real) * C / (2 * np.pi)
-print(f"Frequency {i}: f = {f/1e9:.4f} GHz")
+for i in range(nconv):
+    eigval = eps.getEigenvalue(i)
+    f = np.sqrt(eigval.real) * C / (2 * np.pi)
+    if f < 1e9: continue
+    print(f"Frequency {i}: f = {f/1e9:.4f} GHz")
 
-# Get eigenvector
-vr, _ = A.createVecs()
-eps.getEigenvector(i, vr, _)
-
-Hr = fem.Function(V)
-Hr.x.array[:] = vr.array
-
-Hz = ufl.TrialFunction(V)
-
-# Weak form of the equation
-a = r * Hz.dx(0) * v * dx
-L = r * Hr * v.dx(1) * dx - 1j * r * Hr * v * ds(3)
-
-a = r * dot(grad(u), grad(v)) * dx + \
-    1 / r * u * v * dx - \
-    1j * r * u * v * ds(3)
-
-L = eigval * r * epsilon_r * u * v * dx
-
-boundary_dofs = fem.locate_dofs_topological(V, domain.topology.dim-1, entities=facet_tags.find(3))
-u_bc = fem.Function(V)
-u_bc.x.array[:] = 0
-
-bc = fem.dirichletbc(u_bc, boundary_dofs)
-# bcs = [bc]
-bcs = []
-
-A = fem.petsc.assemble_matrix(fem.form(a))
-A.assemble()
-
-b = fem.petsc.assemble_vector(fem.form(L))
-b.assemble()
-
-ksp = PETSc.KSP().create(V.mesh.comm)
-ksp.setOperators(A)
-
-# Set solver options
-ksp.setType(PETSc.KSP.Type.PREONLY)
-pc = ksp.getPC()
-pc.setType(PETSc.PC.Type.LU)
-pc.setFactorSolverType("mumps")
-
-Hz = fem.Function(V)
-ksp.solve(b, Hz.vector)
-
-# # Create the problem
-# problem = fem.petsc.LinearProblem(a, L, bcs=bcs, petsc_options={
-#     "ksp_type": "preonly",
-#     "pc_type": "lu",
-#     "pc_factor_mat_solver_type": "mumps",
-#     "mat_type": "aij",  # Use this for complex matrices
-# })
-
-# # Solve the problem
-# Hz = problem.solve()
-
-# # Derivative of Hz
-# Hz_dz = ufl.TrialFunction(V)
-# a = r * Hz_dz * v * dx
-# L = r * Hz.dx(0) * v * dx
-
-# problem = fem.petsc.LinearProblem(a, L)
-# Hz_dz = problem.solve()
-
-# # Derivative of Hr
-# Hr_dr = ufl.TrialFunction(V)
-# a = r * Hr_dr * v * dx
-# L = (r * Hr).dx(1) * v * dx
-
-# problem = fem.petsc.LinearProblem(a, L)
-# Hr_dr = problem.solve()
-
-# print(np.column_stack((Hr_dr.x.array, Hz_dz.x.array)).tolist())
-
-topology, cell_types, geometry = vtk_mesh(V)
-grid = pv.UnstructuredGrid(topology, cell_types, geometry)
-plotter = pv.Plotter()
-grid.point_data["u"] = Hz.x.array# Hr_dr.x.array + Hz_dz.x.array
-grid.set_active_scalars("u")
-plotter.add_mesh(grid, show_edges=False)
-
-# Arrows
-H = np.zeros((Hr.x.array.shape[0], 3), dtype=np.complex64)
-H[:, 0] = Hz.x.array
-H[:, 1] = Hr.x.array
+    # Get eigenvector
+    vr, _ = A.createVecs()
+    eps.getEigenvector(i, vr, _)
 
 
-# print(np.column_stack((Hz.x.array, Hr.x.array)).tolist())
+    H = vr.array.reshape(-1, 2)
+    H *= H.shape[0]**0.5 / np.linalg.norm(H, 1)
+    H = np.pad(H, ((0, 0), (0, 1)))
 
-plotter.add_arrows(grid.points, H, mag=1e-15, color="white")  # Adjust scale as needed
+    topology, cell_types, geometry = vtk_mesh(V)
+    grid = pv.UnstructuredGrid(topology, cell_types, geometry)
+    plotter = pv.Plotter()
 
-# Show
-plotter.view_xy()
-if not pv.OFF_SCREEN:
-    plotter.show()
-else:
-    figure = plotter.screenshot("fundamentals_mesh.png")
-# break
+    grid.point_data["u"] = H.real
+    grid.set_active_scalars("u")
+    
+    plotter.add_mesh(grid, show_edges=False, cmap="jet")
+    plotter.add_arrows(grid.points, H.real, mag=1e-2, color="white")
+    
+    # Add in reflection
+    grid = grid.reflect(normal=(0, 1, 0))
+    H[:, 1] *= -1
+    grid.point_data["u"] = H.real
+    grid.set_active_scalars("u")
+    
+    plotter.add_mesh(grid, show_edges=False, cmap="jet")
+    plotter.add_arrows(grid.points, H.real, mag=1e-2, color="white")
+
+    # Show
+    plotter.view_xy()
+    if not pv.OFF_SCREEN:
+        plotter.show()
+    else:
+        figure = plotter.screenshot("fundamentals_mesh.png")
 
 # Clean up
-# A.destroy()
-# M.destroy()
-# b.destroy()
+A.destroy()
+M.destroy()
---
+++ mesh/double.geo
@@ -0,0 +1,63 @@
+// Parameters
+outer_radius = 8.17e-3;
+inner_radius = 4.0e-3;
+height = 1.452e-2;
+gap = 5e-4;
+
+// Mesh size
+lc = 1e-3;
+
+// Resonator - Left
+Point(1) = {-height/2, inner_radius, 0, lc};
+Point(2) = {-height/2, outer_radius, 0, lc};
+Point(3) = {-gap/2, outer_radius, 0, lc};
+Point(4) = {-gap/2, inner_radius, 0, lc};
+
+Line(1) = {1, 2};
+Line(2) = {2, 3};
+Line(3) = {3, 4};
+Line(4) = {4, 1};
+
+Curve Loop(1) = {1, 2, 3, 4};
+Plane Surface(1) = {1};
+
+// Resonator - Right
+Point(5) = {gap/2, inner_radius, 0, lc};
+Point(6) = {gap/2, outer_radius, 0, lc};
+Point(7) = {height/2, outer_radius, 0, lc};
+Point(8) = {height/2, inner_radius, 0, lc};
+
+Line(5) = {5, 6};
+Line(6) = {6, 7};
+Line(7) = {7, 8};
+Line(8) = {8, 5};
+
+Curve Loop(2) = {8, 7, 6, 5};
+Plane Surface(2) = {2};
+Physical Surface("resonator", 1) = {1, 2};
+
+// Air
+// z_max = 3/4 * height;
+// r_max = 3/2 * outer_radius;
+z_max = 1.2e-2;
+r_max = 1.2e-2;
+Point(9) = {-z_max, 0, 0, lc};
+Point(10) = {-z_max, r_max, 0, lc};
+Point(11) = {z_max, r_max, 0, lc};
+Point(12) = {z_max, 0, 0, lc};
+
+Line(9) = {9, 10};
+Line(10) = {10, 11};
+Line(11) = {11, 12};
+Line(12) = {12, 9};
+
+Curve Loop(3) = {9, 10, 11, 12};
+Plane Surface(3) = {3, 1, 2};
+Physical Surface("air", 2) = {3};
+
+// Boundary
+Physical Curve("casing", 3) = {9,10,11};
+Physical Curve("axis", 4) = {12};
+
+// Set output format to mesh
+Mesh.Format = 1;  // .msh format
